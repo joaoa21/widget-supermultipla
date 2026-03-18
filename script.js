d1fd8014fcb4a87577345daafdf46c8fb45c9aea
joaoa21 <joaoams6@gmail.com>
2026-03-18 17:17:18 -0300

    initital commit

---
+++ script.js
@@ -0,0 +1,12 @@
+(function() {
+    const track = document.getElementById('jdoTrack');
+    const SCROLL_AMOUNT = 284; // largura do card + gap
+ 
+    document.querySelector('.jdo-nav-prev').addEventListener('click', function() {
+      track.scrollBy({ left: -SCROLL_AMOUNT, behavior: 'smooth' });
+    });
+ 
+    document.querySelector('.jdo-nav-next').addEventListener('click', function() {
+      track.scrollBy({ left: SCROLL_AMOUNT, behavior: 'smooth' });
+    });
+  })();
