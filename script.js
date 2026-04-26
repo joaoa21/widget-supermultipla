a2b64a99b57c676b81751ba15b1c736a4754306a
joaoa210 <joaoams6@gmail.com>
2026-04-26 00:18:04 -0300

    widgets

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 (function() {
     const track = document.getElementById('jdoTrack');
-    const SCROLL_AMOUNT = 284; // largura do card + gap
+    const SCROLL_AMOUNT = 400; // largura do card + gap
  
     document.querySelector('.jdo-nav-prev').addEventListener('click', function() {
       track.scrollBy({ left: -SCROLL_AMOUNT, behavior: 'smooth' });
